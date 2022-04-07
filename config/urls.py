9fe72a92865a3d6c2df7cc9637f506672c858ba9
Artur <artur.khobotkov@nure.ua>
2022-04-07 22:25:46 +0300

    ПИздец

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("main.urls"))
+    path('', include("main.urls")),
 ]
 
 if settings.DEBUG:
--- main/views.py
+++ main/views.py
@@ -3,14 +3,14 @@ from django.http import HttpResponse
 from django.template.context_processors import request
 from django.views import View
 
-from .models import Post, Category,MyUser
+from .models import Post, Category, MyUser
 
 
 class HomeView(View):
     def get(self, request):
         all_category = Category.objects.values('title', 'slug')
         post_list = Post.objects.all().order_by("-views")[:8]
-        return render(request, 'main/home.html', {'post_list': post_list, 'all_category': all_category})
+        return render(request, 'main/home.html', {'post_list': post_list, 'all_category': all_category,})
 
 
 class PostDetail(View):
